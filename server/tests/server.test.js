e7db488158ff75daa6024eb0d174136ec37de957
Phyo Phyo Lwin <1041577@fairprice.sg>
2018-04-11 18:11:27 +0800

    Add test cases for GET /todo/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
-    text: 'First test todo'
+    text: 'First test todo',
+    _id: new ObjectID()
 },
 {
-    text: 'Second test todo'
+    text: 'Second test todo',
+    _id: new ObjectID()
 }];
 
 //This will be executed before every single test case, only after executing this the test case will be called.
@@ -70,6 +73,33 @@ describe('GET /todos', ()=>{
             .expect((res)=>{
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id',()=>{ 
+    it('should return todo document', (done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found',(done)=>{
+        let hexId = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    if('should return 404 for non-object ids', (done)=>{
+        request(app)
+            .get('/todos/123abc')
+            .expect(404)
             .end(done);
     });
 });
