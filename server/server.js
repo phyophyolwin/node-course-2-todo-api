a1fd1bbed2bdf8f0623d15f5f77d1b2f0c8624bc
Phyo Phyo Lwin <1041577@fairprice.sg>
2018-04-11 17:59:47 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');//take JSON and convert it into object
+const {ObjectID}=require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -25,11 +26,27 @@ app.get('/todos', (req, res)=>{
     Todo.find().then((todos)=>{
         res.send({
             todos
-
         });
     },(err)=>{
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id',(req,res) =>{
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((error)=> {
+        res.status(400).send();
+    });
+           
 });
 
 //GETY /todos/122000 
