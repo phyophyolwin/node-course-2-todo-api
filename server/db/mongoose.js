5367131a70459c3715e4da20f0b616f8c7f1e96a
Phyo Phyo Lwin <1041577@fairprice.sg>
2018-04-12 09:50:59 +0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // setting the promise library to be used by mongoose. global.Promise is the build in promise lib
-mongoose.connect('mongodb://localhost:27017/TodoApp');//connecting the db by mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');//connecting the db by mongoose
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 let app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());//middleware to express
 
@@ -51,8 +52,8 @@ app.get('/todos/:id',(req,res) =>{
 
 //GETY /todos/122000 
 
-app.listen(3000, () =>{
-    console.log('Started on port 3000');
+app.listen(port, () =>{
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
