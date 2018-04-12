eac7450ca73f0c9785912f012849fe638bbe97b3
Phyo Phyo Lwin <1041577@fairprice.sg>
2018-04-12 11:36:28 +0800

    Add test for delete by id

--- server/server.js
+++ server/server.js
@@ -39,11 +39,11 @@ app.delete('/todos/:id',(req,res) =>{
     if(!ObjectID.isValid(id)){
         return res.status(400).send();
     }
-    Todo.findByIdAndRemove(id).then((result) =>{
-        if(result){
+    Todo.findByIdAndRemove(id).then((todo) =>{
+        if(!todo){
             return res.status(404).send();
         }
-        res.send(result);
+        res.send({todo});
     }).catch((err) =>{
         res.status(400).send();
     });
